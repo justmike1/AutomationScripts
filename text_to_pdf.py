40635bb483acb519e5ee4fb65e89c41b3fec2439
justmike1 <90835468+justmike1@users.noreply.github.com>
2021-11-29 16:58:27 +0200

    Create text_to_pdf.py

---
+++ text_to_pdf.py
@@ -0,0 +1,29 @@
+from fpdf import FPDF
+import textwrap
+
+
+def text_to_pdf(text, filename):
+    a3_width_mm = 297
+    pt_to_mm = 0.35
+    fontsize_pt = 10
+    fontsize_mm = fontsize_pt * pt_to_mm
+    margin_bottom_mm = 10
+    character_width_mm = 7 * pt_to_mm
+    width_text = a3_width_mm / character_width_mm
+
+    pdf = FPDF(orientation='P', unit='mm', format='A3')
+    pdf.set_auto_page_break(True, margin=margin_bottom_mm)
+    pdf.add_page()
+    pdf.set_font(family='Courier', size=fontsize_pt)
+    splitted = text.split('\n')
+
+    for line in splitted:
+        lines = textwrap.wrap(line, width_text)
+
+        if len(lines) == 0:
+            pdf.ln()
+
+        for wrap in lines:
+            pdf.cell(0, fontsize_mm, wrap, ln=1)
+
+    pdf.output(filename, 'F')
