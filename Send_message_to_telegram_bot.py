2501106462eed42d9d2265d7b9424fccc1e2742d
justmike1 <90835468+justmike1@users.noreply.github.com>
2021-11-29 19:17:23 +0200

    Create Send_message_to_telegram_bot.py

---
+++ Send_message_to_telegram_bot.py
@@ -0,0 +1,14 @@
+#   You don't need any telegram module or curl scripts
+import requests as r
+
+base_url = "https://api.telegram.org/YOURBOTID/sendDocument"
+my_files = open("FILE.pdf", "rb")
+parameters = {
+  "chat_id": "111111111",     # get your chat id from @useridinfobot
+  "caption": "Good morning everyone!"
+}
+files = {
+  "document": my_files
+}
+resp = r.get(base_url, data=parameters, files=files)
+print(resp.status_code)   # Just to make sure or to monitor errors
