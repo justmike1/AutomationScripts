ab58f65d154a3cc46c7259230699c231264e53c4
justmike1 <90835468+justmike1@users.noreply.github.com>
2021-11-29 19:12:27 +0200

    Create GoogleSheetsTimestamp.js

---
+++ GoogleSheetsTimestamp.js
@@ -0,0 +1,14 @@
+const activeSheet = e.source.getActiveSheet()
+
+const inRange = col >= 2 && col <= 5    // what culumns to check/search? 
+const notStartRow = row >= startRow     // check every row
+const isLog = activeSheet.getName() === "log"     // name of the log inside the sheet (tab)
+
+
+if(
+     inRange &&
+     notStartingRow &&
+     isLog
+) {
+    activeSheet.getRange(row, 7).setValue(new Date())     // if value is changed in those parameters then print timestamp at the end of the row, you can make it on specific cell.
+}
