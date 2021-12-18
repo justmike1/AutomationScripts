8552abd23467a0ac3be3fbcd0885d780e409b01b
justmike1 <90835468+justmike1@users.noreply.github.com>
2021-12-18 21:57:08 +0200

    Update GoogleSheetsTimestamp.js

--- GoogleSheetsTimestamp.js
+++ GoogleSheetsTimestamp.js
@@ -1,16 +1,19 @@
 function onEdit(e) {
-     const activeSheet = e.source.getActiveSheet()
 
-     const inRange = col >= 2 && col <= 5    // what culumns to check/search? 
-     const notStartRow = row >= startRow     // check every row
-     const isLog = activeSheet.getName() === "log"     // name of the log inside the sheet (tab)
+  var startRow = 1;
+  var row = e.range.getRow();
+  var col = e.range.getColumn();
 
+  const activeSheet = e.source.getActiveSheet()
+  const inRange = col >= 1 && col <= 10    // what culumns to check/search? 
+  const notStartingRow = row >= startRow     // check every row
+  const isLog = activeSheet.getName() === "log"     // name of the log inside the sheet (tab)
 
-     if(
-          inRange &&
-          notStartingRow &&
-          isLog
-     ) {
-         activeSheet.getRange(row, 7).setValue(new Date())     // if value is changed in those parameters then print timestamp at the end of the row, you can make it on specific cell.
-     }
+  if(
+      inRange &&
+      notStartingRow &&
+      isLog
+  ) {
+      activeSheet.getRange(row, 11).setValue(new Date())     // if value is changed in those parameters then print timestamp at the end of the row, you can make it on specific cell.
+  }
 }
