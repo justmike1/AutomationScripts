7b65959a68553efa85b67beb095ce376e07dfe0a
justmike1 <90835468+justmike1@users.noreply.github.com>
2021-12-23 16:37:38 +0200

    Update GoogleSheetsTimestamp.js

--- GoogleSheetsTimestamp.js
+++ GoogleSheetsTimestamp.js
@@ -17,3 +17,28 @@ function onEdit(e) {
       activeSheet.getRange(row, 11).setValue(new Date())     // if value is changed in those parameters then print timestamp at the end of the row, you can make it on specific cell.
   }
 }
+// EXAMPLE:
+// function onEdit(e) {
+
+//   var startRow = 1;
+  
+//   var row = e.range.getRow();
+//   var col = e.range.getColumn();
+
+//   if(col >= 2 && col <= 6 && row >= startRow && e.source.getActiveSheet().getName() === "Log about stuff 1"){
+//     e.source.getActiveSheet().getRange(row,8).setValue(new Date());
+//   }
+
+//   if(col >= 1 && col <= 11 && row >= startRow && e.source.getActiveSheet().getName() === "Log about other stuff"){
+//     e.source.getActiveSheet().getRange('B1').setValue(new Date());
+//   }
+
+//   if(col >= 1 && col <= 5 && row >= startRow && e.source.getActiveSheet().getName() === "useless log"){
+//     e.source.getActiveSheet().getRange('B1').setValue(new Date());
+//   }
+
+//   if(col === 3 && row >= startRow && e.source.getActiveSheet().getName() === "i.d.e.n"){
+//     e.source.getActiveSheet().getRange(row,1).setValue(new Date());
+//   }
+
+// }
